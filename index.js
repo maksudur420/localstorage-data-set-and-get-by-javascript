0cd01bc8c41f3b3bc371c0d0ce65709f11df2a35
maksudur420 <mdmaksudur588@gmail.com>
2023-09-11 16:31:00 +0600

    commited all process for localstorage data stored by JavaScript

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const addProduct =()=>{
 
     
     displayProduct(productName,productQuantity);
+    saveTotalStoreProduct (productName,productQuantity)
 
 }
 
@@ -19,3 +20,31 @@ const displayProduct=(Name,Quantity)=>{
     li.innerText = `${Name}:${Quantity}`;
     container.appendChild(li);
 }
+
+const getStoredShoppingCart =()=>{
+    let cart = {};
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+
+}
+
+const saveTotalStoreProduct =(product,quantity)=>{
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const productStringify = JSON.stringify(cart);
+    localStorage.setItem('cart',productStringify)
+}
+
+const displayStoreCart =() =>{
+    const browserData = getStoredShoppingCart();
+    console.log(browserData)
+    for (const product in browserData) {
+        const quantity = browserData[product ];
+        displayProduct(product,quantity);
+
+    }
+}
+displayStoreCart();
